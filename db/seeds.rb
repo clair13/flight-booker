eb40e2efaccacc172f0ca012cb53161380134075
clair13 <theclairvoyant1313@yahoo.gr>
2019-09-23 18:06:09 +0300

    Fix model bug and add more logical seeded datetimes

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,13 @@ list_of_airports = Airport.all
 10000.times do
   departure_airport = list_of_airports[rand(0..airports.length - 1)].id
   arrival_airport = list_of_airports[rand(1..airports.length - 1)].id
-  Flight.create(departure_date: rand(60.days).seconds.from_now,
-                arrival_date: rand(60.days).seconds.from_now,
+  random_departure_date = rand(60.days).seconds.from_now
+  random_arrival_date = (random_departure_date.to_time + rand(4.hours)).to_datetime
+  Flight.create(departure_date: dd,
+                arrival_date: ad,
                 departure_airport_id: departure_airport,
                 arrival_airport_id: arrival_airport,
                 flight_number: rand(1000..9999))
 end
+
+p "seeds done"
