c462965f32480333a9b67884191bfa31d18390e3
clair13 <theclairvoyant1313@yahoo.gr>
2019-11-09 20:33:42 +0200

    Display flights on index page

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: :arrival_airport_id
 
   has_many :bookings
+  has_many :passenger_bookings, through: :bookings
   has_many :passengers, through: :passenger_bookings
 
   def self.dates_list
