d6e99ba9a045f60d08dab753039f239b1e9984de
clair13 <theclairvoyant1313@yahoo.gr>
2019-11-09 20:20:17 +0200

    Fix bug in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ list_of_airports = Airport.all
   arrival_airport = list_of_airports[rand(1..airports.length - 1)].id
   random_departure_date = rand(60.days).seconds.from_now
   random_arrival_date = (random_departure_date.to_time + rand(4.hours)).to_datetime
-  Flight.create(departure_date: dd,
-                arrival_date: ad,
+  Flight.create(departure_date: random_departure_date,
+                arrival_date: random_arrival_date,
                 departure_airport_id: departure_airport,
                 arrival_airport_id: arrival_airport,
                 flight_number: rand(1000..9999))
