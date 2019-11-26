716f5b08a705144f4c6d0a29b2c4069a05fd915e
clair13 <theclairvoyant1313@yahoo.gr>
2019-11-26 19:35:26 +0200

    Finish bookings controller

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.5'
+gem 'byebug'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,21 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
+    params[:passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    booking = Booking.create(booking_params)
+    if booking
+      redirect_to booking
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id,passenger_attributes: [:name, :email]
   end
 end
